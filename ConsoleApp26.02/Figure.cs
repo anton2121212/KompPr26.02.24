d7c4dd7bc9135013eb682f90a2447be6aa02f9b3
anton2121212 <122678473+anton2121212@users.noreply.github.com>
2024-02-26 10:49:55 +0300

    Add files via upload

---
+++ ConsoleApp26.02/Figure.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26._02
+{
+    /// <summary>
+    /// Разработать абстрактный класс Figure, в котором определить следующие элементы:
+    /// </summary>
+    abstract class Figure
+    {
+        /// <summary>
+        /// скрытое внутреннее поле name (название фигуры);
+        /// </summary>
+        string name;
+
+        /// <summary>
+        /// конструктор без параметров 
+        /// </summary>
+        public Figure() { }
+
+
+        /// <summary>
+        /// конструктор с одним параметром, инициализирующий поле name указанным значением;
+        /// </summary>
+        /// <param name="name"></param>
+        public Figure (string name)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// свойство Name для доступа к внутреннему полю name;
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// абстрактный метод Area(), предназначенный для получения площади фигуры
+        /// </summary>
+        public abstract double Area();
+
+
+        /// <summary>
+        ///  абстрактный метод Perimeter(), предназначенный для получения периметра фигуры
+        /// </summary>
+        public abstract double Perimeter();
+
+
+        /// <summary>
+        /// Виртуальный метод для вывода
+        /// </summary>
+        virtual public void Print()
+        {
+            Console.WriteLine($"Название фигуры: {name}");
+        }
+        
+
+    }
+}
---
+++ ConsoleApp26.02/Rectangle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26._02
+{
+    /// <summary>
+    /// Производный класс Rectangle который наследует возможности класса Figure
+    /// </summary>
+    class Rectangle:Figure
+    {
+        /// <summary>
+        /// Скрытые внутренние поля
+        /// </summary>
+        int side1;
+        int side2;
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Rectangle() { }
+
+        /// <summary>
+        /// Конструтор с параметрами
+        /// </summary>
+        /// <param name="side1"></param>
+        /// <param name="side2"></param>
+        public Rectangle(string name,int side1, int side2):base(name) 
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        /// <summary>
+        /// Свойства к полям для доступа к ним
+        /// </summary>
+        public int Side1
+        { 
+            get { return side1; }
+            set { side1 = value; }
+        }
+        public int Side2
+        {
+            get { return side2; } 
+            set { side2 = value; }
+        }
+
+        /// <summary>
+        /// метод Area(), возвращающий площадь прямоугольника по его сторонам; 
+        /// </summary>
+        /// <returns></returns>
+        public override double Area()
+        {
+            return side1 * side2;
+        }
+
+
+        /// <summary>
+        /// метод Perimeter(), возвращающий периметр прямоугольника по его сторонам;
+        /// </summary>
+        /// <returns></returns>
+        public override double Perimeter()
+        {
+            return (side1+side2)*2;
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Имя фигуры: {Name}");
+        }
+
+
+
+
+    }
+}
